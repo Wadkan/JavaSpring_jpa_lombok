3f374465ff80dc50536d1e86a2b30ec746720fb3
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-05-31 11:40:23 +0200

    h2 created

---
+++ src/main/java/com/jpa_lombok_codecook_again/demo/JpaIntroApplication.java
@@ -0,0 +1,13 @@
+package com.jpa_lombok_codecook_again.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaIntroApplication.class, args);
+    }
+
+}
