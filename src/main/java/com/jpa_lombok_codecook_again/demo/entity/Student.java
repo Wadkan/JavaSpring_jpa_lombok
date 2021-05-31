3d27f964808671c2dc2d3c5ab3820ace0a4b3254
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-05-31 14:13:19 +0200

    all test work with Student, before create Address class

--- src/main/java/com/jpa_lombok_codecook_again/demo/entity/Student.java
+++ src/main/java/com/jpa_lombok_codecook_again/demo/entity/Student.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Data
 @NoArgsConstructor
@@ -22,9 +22,17 @@ public class Student {
 
     private String name;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
     private LocalDate birthDate;
 
+    @Transient
     private long age;
+
+    public void calculateAge() {
+        if (birthDate != null) {
+            age = ChronoUnit.YEARS.between(birthDate, LocalDate.now());
+        }
+    }
 }
--- src/test/java/com/jpa_lombok_codecook_again/demo/repository/AllRepositoryTest.java
+++ src/test/java/com/jpa_lombok_codecook_again/demo/repository/AllRepositoryTest.java
@@ -6,9 +6,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.xml.bind.DatatypeConverterInterface;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -38,4 +41,46 @@ public class AllRepositoryTest {
         assertTrue(studentList.size() == 1);
     }
 
+    @Test(expected = DataIntegrityViolationException.class)
+    public void saveUniqueFieldTwice() {
+        Student student = Student.builder()
+                .email("john@lcodecoo.com")
+                .name("John")
+                .build();
+
+        studentRepository.save(student);
+
+        Student student2 = Student.builder()
+                .email("john@lcodecoo.com")
+                .name("Peter")
+                .build();
+
+        studentRepository.saveAndFlush(student2);
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void emailShouldBeNotNull() {
+        Student student2 = Student.builder()
+                .name("Peter")
+                .build();
+
+        studentRepository.saveAndFlush(student2);
+    }
+
+    @Test
+    public void transientIsNotSaved() {
+        Student student = Student.builder()
+                .birthDate(LocalDate.of(1987, 02, 12))
+                .email("john@lcodecoo.com")
+                .name("Peter")
+                .build();
+        student.calculateAge();
+        assertTrue(student.getAge() >= 31);
+
+        studentRepository.save(student);
+        entityManager.clear();
+
+        List<Student> students = studentRepository.findAll();
+        assertTrue(students.stream().allMatch(student1 -> student1.getAge() == 0L));
+    }
 }
